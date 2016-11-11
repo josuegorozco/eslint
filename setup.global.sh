75d87d941f83ccc2314470a2a6e928a6fa11f6d4
Josue Orozco <jorozco@surgeforward.com>
2016-11-10 16:34:37 -0800

    Adding latest rules

---
+++ setup.global.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+npm install -g \
+    babel-cli@latest \
+    babel-core@latest \
+    babel-eslint@latest \
+    babel-loader@latest \
+    babel-plugin-istanbul@latest \
+    babel-plugin-react-intl@latest \
+    babel-plugin-react-transform@latest \
+    babel-plugin-transform-react-constant-elements@latest \
+    babel-plugin-transform-react-inline-elements@latest \
+    babel-plugin-transform-react-remove-prop-types@latest \
+    babel-preset-latest@latest \
+    babel-preset-react@latest \
+    babel-preset-react-hmre@latest \
+    babel-preset-stage-0@latest \
+    eslint@latest \
+    eslint-config-airbnb@latest \
+    eslint-config-airbnb-base@latest \
+    eslint-import-resolver-webpack@latest \
+    eslint-plugin-import@latest \
+    eslint-plugin-jsx-a11y@latest \
+    eslint-plugin-react@latest \
+    eslint-plugin-redux-saga@latest \
+    redux-saga@latest \
+    jscs@latest \
+    js-beautify@latest \
+    sass-convert@latest
